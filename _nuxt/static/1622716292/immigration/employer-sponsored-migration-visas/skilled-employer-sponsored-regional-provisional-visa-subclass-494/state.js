95aa368c8d2d93a7c081ad7c9df144c77c07fb0f
Kashif Akram <k.akram@outlook.cm>
2021-06-03 21:01:02 +1000

    family fixes

--- _nuxt/static/1622716292/immigration/employer-sponsored-migration-visas/skilled-employer-sponsored-regional-provisional-visa-subclass-494/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622716292",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimmigration\u002Femployer-sponsored-migration-visas\u002Fskilled-employer-sponsored-regional-provisional-visa-subclass-494",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1622718021/test/testme/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622718021",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftest\u002Ftestme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
