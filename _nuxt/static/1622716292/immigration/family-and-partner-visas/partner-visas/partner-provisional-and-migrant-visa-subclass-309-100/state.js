ca92d5be9578bf0a684c7c6aaab8acf12a4f785c
Kashif Akram <k.akram@outlook.cm>
2021-06-03 20:32:37 +1000

    link color fixes

---
+++ _nuxt/static/1622716292/immigration/family-and-partner-visas/partner-visas/partner-provisional-and-migrant-visa-subclass-309-100/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622716292",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimmigration\u002Ffamily-and-partner-visas\u002Fpartner-visas\u002Fpartner-provisional-and-migrant-visa-subclass-309-100",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1622716292/immigration/temporary-work-visas/training-visa-subclass-407/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622716292",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimmigration\u002Ftemporary-work-visas\u002Ftraining-visa-subclass-407",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
