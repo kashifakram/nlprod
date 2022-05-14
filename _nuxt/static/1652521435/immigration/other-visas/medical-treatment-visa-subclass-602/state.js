b47b681c36f38857a4a61dadf5b729bfbb9298f7
Kashif Akram <kashif@kashifakram.com>
2022-05-14 19:44:53 +1000

    spacings

---
+++ _nuxt/static/1652521435/immigration/other-visas/medical-treatment-visa-subclass-602/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652521435",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimmigration\u002Fother-visas\u002Fmedical-treatment-visa-subclass-602",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
