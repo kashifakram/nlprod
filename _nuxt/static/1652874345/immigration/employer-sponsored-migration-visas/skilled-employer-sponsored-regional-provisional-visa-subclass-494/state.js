64d0b7b38c6ffe9cc115cdcddebf710fa52c1bca
Kashif Akram <kashif@kashifakram.com>
2022-05-18 21:48:04 +1000

    mobile

---
+++ _nuxt/static/1652874345/immigration/employer-sponsored-migration-visas/skilled-employer-sponsored-regional-provisional-visa-subclass-494/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652874345",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimmigration\u002Femployer-sponsored-migration-visas\u002Fskilled-employer-sponsored-regional-provisional-visa-subclass-494",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
